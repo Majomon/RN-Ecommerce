91d4d9e00e8742a18436eb87ec05bd8955e5e9be
Mauri <mauri.monzon91@gmail.com>
2024-09-02 22:06:57 -0300

    Theme

---
+++ src/config/index.ts
@@ -0,0 +1,1 @@
+import {COLORS, SIZES} from './theme';
---
+++ src/config/theme.ts
@@ -0,0 +1,30 @@
+import {Dimensions} from 'react-native';
+
+const {width, height} = Dimensions.get('window');
+
+export const COLORS = {
+  primary: '#2A4D50',
+  secondary: '#DDF0FF',
+  tertiary: '#FF7754',
+
+  gray: '#83829A',
+  gray2: '#C1C0C8',
+
+  offwhite: '#F3F4F8',
+  white: '#FFFFFF',
+  black: '#000000',
+  red: '#e81e4d',
+  green: '#00C135',
+  lightWhite: '#FAFAFC',
+};
+
+export const SIZES = {
+  xSmall: 10,
+  smasll: 12,
+  medium: 16,
+  large: 20,
+  xLarge: 24,
+  xxLarge: 44,
+  width,
+  height,
+};
