a56908f1abf1d1cf55ab8de3e6cd3b6597f6d394
Mauri <mauri.monzon91@gmail.com>
2024-09-06 09:49:08 -0300

    ProductCardView

--- src/presentation/components/products/productCardView/ProductCardView.style.ts
+++ src/presentation/components/products/productCardView/ProductCardView.style.ts
@@ -4,9 +4,17 @@ import {COLORS, SIZES} from '../../../../config/theme';
 export const styleProductCard = StyleSheet.create({
   container: {
     width: 182,
-    height: 240,
+    height: 200,
     marginEnd: 22,
     borderRadius: SIZES.medium,
     backgroundColor: COLORS.secondary,
+  },
+  imageContainer: {
+    flex: 1,
+    width: 170,
+    marginLeft: SIZES.small / 2,
+    marginTop: SIZES.small / 2,
+    borderRadius: SIZES.small,
+    overflow: 'hidden',
   },
 });
--- src/presentation/components/products/productCardView/ProductCardView.tsx
+++ src/presentation/components/products/productCardView/ProductCardView.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
-import {Pressable, Text, View} from 'react-native';
+import {Image, Pressable, Text, View} from 'react-native';
 import {styleProductCard} from './ProductCardView.style';
 
 export const ProductCardView = () => {
+  const slides = [
+    'https://www.parati.com.ar/wp-content/uploads/2022/10/ALE_3255-749x561.jpg.webp',
+    'https://bucket.somosohlala.com.ar/s3fs-public/styles/internal_990/public/2022-08/15.png.webp',
+    'https://beca.pe/wp-content/uploads/2023/12/d195186plex-1080x675.webp',
+  ];
   return (
     <Pressable onPress={() => {}}>
-      <View style={styleProductCard.container}></View>
+      <View style={styleProductCard.container}>
+        <View style={styleProductCard.imageContainer}>
+          <Image
+            source={{
+              uri: 'https://beca.pe/wp-content/uploads/2023/12/d195186plex-1080x675.webp',
+            }}
+            style={}
+          />
+        </View>
+      </View>
     </Pressable>
   );
 };
