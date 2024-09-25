1c8a523898a1f8d19abe940a95860336ff9476ea
Mauri <mauri.monzon91@gmail.com>
2024-09-24 23:05:26 -0300

    hooks

--- src/presentation/components/products/productsRow/ProductsRow.tsx
+++ src/presentation/components/products/productsRow/ProductsRow.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import {FlatList, Text, View} from 'react-native';
+import {FlatList, View} from 'react-native';
 import {SIZES} from '../../../../config/theme';
+import {useFetch} from '../../../hooks/useFetch';
 import {ProductCardView} from '../productCardView/ProductCardView';
 import {styleProductsRow} from './ProductsRow.style';
 
 export const ProductsRow = () => {
+  const {data, isLoading, error} = useFetch();
   const products = [1, 2, 3, 4];
   return (
     <View style={styleProductsRow.container}>
       <FlatList
         horizontal
-        data={products}
+        data={data}
         contentContainerStyle={{columnGap: SIZES.medium}}
         renderItem={({item}) => <ProductCardView />}
       />
---
+++ src/presentation/hooks/useFetch.tsx
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import {useEffect, useState} from 'react';
+
+export const useFetch = () => {
+  const [data, setData] = useState([]);
+  const [isLoading, isSetLoading] = useState(false);
+  const [error, setError] = useState<null | string>(null);
+
+  const fetchData = async () => {
+    isSetLoading(true);
+    try {
+      const response = await axios.get('http://localhost:3000/api/products');
+      setData(response.data);
+      isSetLoading(false);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        setError(error.message);
+      } else {
+        setError('An unexpected error occurred');
+      }
+      isSetLoading(false);
+    } finally {
+      isSetLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return {data, isLoading, error, fetchData};
+};
